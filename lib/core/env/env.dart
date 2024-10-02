b37a4abe7ee546e8bbd5d06b27374481a154666b
deny <denyjoefakri@gmail.com>
2024-10-03 03:25:59 +0700

    Set Env

--- lib/core/env/env.dart
+++ lib/core/env/env.dart
@@ -1,4 +1,6 @@
 
 
-const String baseUrlString = "wss://ws.eodhistoricaldata.com/ws/crypto?api_token=demo";
-const String symbols = "BTC-USD, ETH-USD";
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+final String baseUrlString = dotenv.env['BASE_URL']!;
+final String symbols = dotenv.env['SYMBOLS']!;
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'core/injection/dependency_injection.dart';
 
 
 void main() async {
-  // await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
